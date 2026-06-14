c7847adaead57a015e958e9bc047452f7b2b22ec
clydejallorina <clydejallorina@gmail.com>
2026-06-14 10:29:55 +0800

    Force SessionDate string saved to be from invariant culture

--- CustomLeaderboard/GlobalLeaderboard.cs
+++ CustomLeaderboard/GlobalLeaderboard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using BaboonAPI.Hooks.Tracks;
@@ -272,7 +273,7 @@ namespace TootTallyLeaderboard
                         if (Plugin.Instance.option.SessionStartTT.Value == 0 || Plugin.Instance.ShouldUpdateSession)
                         {
                             Plugin.Instance.ShouldUpdateSession = false;
-                            Plugin.Instance.option.SessionDate.Value = DateTime.Now.AddDays(1).ToString();
+                            Plugin.Instance.option.SessionDate.Value = DateTime.Now.AddDays(1).ToString(CultureInfo.InvariantCulture);
                             Plugin.Instance.option.SessionStartTT.Value = user.tt;
                         }
 
